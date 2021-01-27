95a1376c0c444b639bb538005897a2b1af4d31ea
kyumin <cgm97@naver.com>
2021-01-27 17:56:36 +0900

    일정날짜 표시 오류 수정

--- src/main/java/com/kyumin/calendar/service/CalendarServiceImpl.java
+++ src/main/java/com/kyumin/calendar/service/CalendarServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kyumin.calendar.service;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -18,34 +19,38 @@ public class CalendarServiceImpl implements CalendarService {
 	private CalendarDTO calendarDTO;
 	@Autowired
 	private CalendarRepository dao;
-
+	
+	private SimpleDateFormat change = new SimpleDateFormat("yyyy-MM-dd");
+	private Calendar cal = Calendar.getInstance();
+	
 	@Override
 	public void clickDate(Date selectedDate, Model model) {
-		SimpleDateFormat change = new SimpleDateFormat("yyyy-MM-dd");
 		String startDate = change.format(selectedDate);
 
 		calendarDTO.setStartDate(startDate);
 		model.addAttribute("selectedCalendar", calendarDTO);
 	}
-
+	
+	// 일정 목록 추가
 	@Override
-	public void writeCalendar(CalendarDTO dto) {
-		System.out.println("제목 : " + dto.getTitle());
-		System.out.println("시작 : " + dto.getStartDate());
-		System.out.println("종료 : " + dto.getEndDate());
-		System.out.println("내용 : " + dto.getContent());
+	public void writeCalendar(CalendarDTO dto) throws Exception{
+		/*
+       * 2021-01-25 부터 27 까지로 선택하여 일정 추기시 화면상에 26 일까지로 표시 되는 문제를 해결하기 위해  +1일
+		 */
+		cal = Calendar.getInstance();
+		Date before_endDate = change.parse(dto.getEndDate());
 		
+		cal.setTime(before_endDate);
+		cal.add(Calendar.DATE, 1);
+		String after_endDate = change.format(cal.getTime());
+		
+		dto.setEndDate(after_endDate);
 		dao.insertCalendar(dto);
 	}
-
+	
+	// 일정 목록 불러오기
 	@Override
 	public List<CalendarDTO> showCalendar() throws Exception {
-		/*
-		 * List<CalendarDTO> list = new ArrayList<CalendarDTO>();
-		 * calendarDTO.setTitle("aaa"); calendarDTO.setStartDate("2021-01-25");
-		 * list.add(calendarDTO); // -> dao 로 이동 해야함
-		 */
-	
 		return dao.getCalendar();
 	}
 }
