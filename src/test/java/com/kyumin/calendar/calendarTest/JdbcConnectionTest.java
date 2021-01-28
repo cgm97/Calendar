9e8768a75ad6dbebcf8dbb9f725dbc09dacad747
kyumin <cgm97@naver.com>
2021-01-28 19:01:57 +0900

    DataSourceTest

--- src/test/java/com/kyumin/calendar/calendarTest/JdbcConnectionTest.java
+++ src/test/java/com/kyumin/calendar/calendarTest/JdbcConnectionTest.java
@@ -1,50 +1,35 @@
 package com.kyumin.calendar.calendarTest;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.junit.Assert;
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.kyumin.calendar.domain.CalendarDTO;
+public class JdbcConnectionTest {     
+	
+	private DataSource dataSource;
+	
+//	public void setDataSource(DataSource dataSource) {
+//		this.dataSource = dataSource;
+//	}
 
-public class JdbcConnectionTest {
-	private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	private static final String URL = "jdbc:oracle:thin:@localhost:1521/xe";   
-	private static final String USER = "C##CALENDAR";         
-	private static final String PW = "4495";       
-	private Connection conn;
-	private Statement stmt;
-	private PreparedStatement pstmt;
-	private ResultSet rs;
-	private ArrayList<CalendarDTO> list;
 	@Test
 	public  void Jdbc() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:src/main/webapp/WEB-INF/spring/service-context.xml");
+		dataSource = (DataSource) ac.getBean("dataSource");
 			try {
-				Class.forName(DRIVER);
-				Connection conn = DriverManager.getConnection(URL, USER, PW);
-				stmt = conn.createStatement();
+				Connection conn = dataSource.getConnection();
 				
-				String sql = "select * from CALENDAR";
-				rs = stmt.executeQuery(sql);
-				list = new ArrayList<CalendarDTO>();
-				String a = null;
 				System.out.print(conn);
-				while (rs.next()) {
-					CalendarDTO dto = new CalendarDTO();
-					dto.setTitle(rs.getString("TITLE"));
-					dto.setStartDate(rs.getString("STARTDATE"));
-					dto.setEndDate(rs.getString("ENDDATE"));
-					dto.setContent(rs.getString("CONTENT"));
-				}
-				System.out.print(a);
-			} catch (ClassNotFoundException | SQLException e) {
+
+			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
